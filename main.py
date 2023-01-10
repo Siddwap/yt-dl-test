843b25bb7bf9a88f8c37c61d0d569bcbe1022ed6
Siddwap <58450503+Siddwap@users.noreply.github.com>
2023-01-10 18:58:23 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request
+
+import youtube_dl
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def index():
+
+    return render_template("index.html")
+
+@app.route("/download", methods=["POST"])
+
+def download():
+
+    url = request.form["url"]
+
+    ydl_opts = {
+
+        "format": "bestvideo[height<=480]+bestaudio/best[height<=480]",
+
+        "outtmpl": "%(title)s.%(ext)s",
+
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+
+        info = ydl.extract_info(url, download=False)
+
+        formats = info.get("formats", [])
+
+    return render_template("download.html", formats=formats, url=url)
+
+if __name__ == "__main__":
+
+    app.run(debug=True)
+
